70f6450a3c5dd0b2c0b0aad5d7431988960697b5
Akele Belay <akelebch12@gmail.com>
2022-07-29 09:21:08 +0000

    Unused imports removed for better code readability

--- app.py
+++ app.py
@@ -3,18 +3,13 @@
 #----------------------------------------------------------------------------#
 
 from datetime import datetime
-import json
 import dateutil.parser
 import babel
-from flask import (Flask, render_template, request,
-                   Response, flash, redirect, url_for)
+from flask import (render_template, request, flash, redirect, url_for)
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from flask_wtf import Form
 from forms import *
-from flask_migrate import Migrate
 import sys
 from model import app, db, Venue, Artist, Show
 import collections
